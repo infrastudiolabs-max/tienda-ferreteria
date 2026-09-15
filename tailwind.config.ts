b3bd66f772cb38766792c5fe6412b21d4a9b8452
infrastudiolabs-max <infrastudiolabs@gmail.com>
2026-09-15 02:02:47 -0500

    Setup: Agregar tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F97316',
+        secondary: '#0F172A',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
